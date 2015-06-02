5237bdf521321ca21118931999f96569bd37b9d5
badqiu <badqiu@gmail.com>
2015-06-02 16:48:36 +0800

    add jsoup for download html2text

--- src/main/java/com/github/reportengine/util/ViewUtils.java
+++ src/main/java/com/github/reportengine/util/ViewUtils.java
@@ -9,6 +9,8 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.beanutils.NestedNullException;
+import org.apache.commons.lang.StringUtils;
+import org.jsoup.Jsoup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -69,83 +71,6 @@ public class ViewUtils {
 		DecimalFormat df=new DecimalFormat(preStr.toString()); 
 		return percent(number1,number2,df);
 	}
-	
-	/**
-	 * top n
-	 * @param rows
-	 * @param column
-	 * @param n
-	 * @return
-	 */
-	public static List<Map<String,Object>> topN(List<Map<String,Object>> rows,final String column, int n) {
-		List<Map<String,Object>> temp = cloneList(rows);
-		Collections.sort(temp, new Comparator<Map<String,Object>>() {
-			public int compare(Map<String, Object> row1, Map<String, Object> row2) {
-				try {
-					Object o1 = PropertyUtils.getNestedProperty(row1, column);
-					Object o2 = PropertyUtils.getNestedProperty(row2, column);
-					if (o1 == null) {
-						return 1;
-					}
-					if (o2 == null) {
-						return -1;
-					}
-					int num1 = Integer.valueOf(o1.toString());
-					int num2 = Integer.valueOf(o2.toString());
-					return num2 - num1;
-				} catch (NestedNullException e) {
-					return -1;
-				}
-			}
-		});
-		if(rows.size()<=n) {
-			return temp;
-		}
-		List<Map<String,Object>> result = new ArrayList<Map<String,Object>>();
-		for(int i=0;i<n;i++) {
-			result.add(temp.get(i));
-		}
-		return result;
-	}
-	
-	public static List<Map<String,Object>> cloneList(List<Map<String,Object>> list) {
-		List<Map<String,Object>> temp = new ArrayList<Map<String,Object>>();
-		for(Map map : list) {
-			temp.add(map);
-		}
-		return temp;
-	}
-	
-	/**
-	 * 求sum
-	 * sum(rows, "kcnts.user_cnt","kcnt.abc_cnt")
-	 * @param rows
-	 * @param columns
-	 * @return
-	 */
-	public static Map<String,Double> sum(List<Map<String,Object>> rows,final String... columns) {
-		Map<String,Double> result = new HashMap<String,Double>();
-		for(Map row : rows) {
-			for(int i=0;i<columns.length;i++) {
-				String key = columns[i];
-				try {
-					Object value = PropertyUtils.getNestedProperty(row, columns[i]);
-					if(value != null) {
-						Double oldValue = result.get(key);
-						if(oldValue == null) {
-							oldValue = 0.0;
-						}
-						result.put(key, oldValue + Double.parseDouble(value.toString()));
-					}
-				}catch(NestedNullException e) {
-					//ignore
-				}
-			}		
-			
-		}
-		return result;
-	}
-	
 	
 	/**
 	 * 转化率
@@ -234,112 +159,12 @@ public class ViewUtils {
 		return result+"%";
 	}
 	
-	public String objectToString(Object obj) {
-		if (obj == null) {
-			return "";
+
+	public static String html2text(String html) {
+		if(StringUtils.isBlank(html)) {
+			return html;
 		}
-		return obj.toString();
+		return Jsoup.parse(html).text();
 	}
-	
-	/**
-	 * 合并两个数据集
-	 * @param rows1
-	 * @param rows2
-	 * @param combineColumens
-	 * @param alias
-	 * @param innerColumns
-	 * @return
-	 */
-	public static List<Map<String,Object>> combine(List<Map<String,Object>> rows1,List<Map<String,Object>> rows2,String[] combineColumens,String[] alias,String[] innerColumns){
-		 if(!(rows1 instanceof java.util.ArrayList)) {
-			 rows1 = new ArrayList<Map<String,Object>>();
-		 }
-	    for(Map row1:rows1) {
-	    boolean flag = true;
-	       for(Map row2 :rows2) {
-	         if(isJoin(row1,row2,innerColumns)) {
-	        	 for(int i=0;i<combineColumens.length;i++) {
-	  			   row1.put(alias[i], row2.get(combineColumens[i]));
-	  			  }
-	           rows2.remove(row2);
-	           flag = false;
-	           break;
-	         }
-	       }
-	       if(flag) {
-	    	   for(int i=0;i<combineColumens.length;i++) {
-	    		   copyDataStructure(row1,combineColumens[i],alias[i]);
-	  		   }  
-	       }
-	    }
-	   for(Map row : rows2) {
-		 for(int i=0;i<combineColumens.length;i++) {
-			 row.put(alias[i], row.get(combineColumens[i]));
-			 copyDataStructure(row,alias[i],combineColumens[i]);
-  		 }  
-	     ((ArrayList)rows1).add(row);
-	   }
-	   return rows1;
-	}
-	/**
-	 * 把第二数据集合并到第一个
-	 * @param rows1
-	 * @param rows2
-	 * @param combineColumens
-	 * @param alias
-	 * @param innerColumns
-	 * @return
-	 */
-	public static List<Map<String,Object>> mergeTo(List<Map<String,Object>> rows1,List<Map<String,Object>> rows2,String[] combineColumens,String[] alias,String[] innerColumns){
-		 if(!(rows1 instanceof java.util.ArrayList)) {
-			 rows1 = new ArrayList<Map<String,Object>>();
-		 }
-	    for(Map row1:rows1) {
-	    boolean flag = true;
-	       for(Map row2 :rows2) {
-	         if(isJoin(row1,row2,innerColumns)) {
-	        	 for(int i=0;i<combineColumens.length;i++) {
-	  			   row1.put(alias[i], row2.get(combineColumens[i]));
-	  			  }
-	           rows2.remove(row2);
-	           flag = false;
-	           break;
-	         }
-	       }
-	       if(flag) {
-	    	   for(int i=0;i<combineColumens.length;i++) {
-	    		   copyDataStructure(row1,combineColumens[i],alias[i]);
-	  		   }  
-	       }
-	    }
-	   return rows1;
-	}
-	
-	private static boolean isJoin(Map row1,Map row2,String[] innerColumns) {
-	   for(String column:innerColumns) {
-		   Object colValue1 = row1.get(column);
-		   Object colValue2 = row2.get(column);
-		   if(colValue1 == null && colValue1 == null) {
-			   continue;
-		   }else if(colValue1 == null || colValue1 == null){
-			   return false;
-		   }else {
-			   if(!colValue1.equals(colValue2)) {
-				  return false;
-			   }
-		   }
-	   }
-	   return true;
-	}
-	
-	private static void copyDataStructure(Map row, String column, String alias) {
-		Object o = row.get(column);
-		if(o instanceof Integer || o instanceof Long || o instanceof Double || o instanceof Float) {
-			row.put(alias, 0);
-		} else if(o instanceof String) {
-			row.put(alias, "");
-		} else if(o instanceof java.util.HashMap) {
-			row.put(alias, new HashMap());
-		}
-	}
+
 }
--- src/test/resources/report_conf/download.ftl
+++ src/test/resources/report_conf/download.ftl
@@ -7,7 +7,7 @@
 		<#local sum = context[table.refDataList + 'Sum']! /> 
 	</#if>
 	<#list table.dataList as row>
-		<#list table.columns as col><#assign rowValue = col.value?interpret><@rowValue /><#if col_has_next>,</#if></#list>
+		<#list table.columns as col><#assign rowValue = col.value?interpret><#assign tmpText><@rowValue /></#assign>${ViewUtils.html2text(tmpText)}<#if col_has_next>,</#if></#list>
 	</#list>
 	</#compress>
 </#macro>
